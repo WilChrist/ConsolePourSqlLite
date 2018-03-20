ba474146c5caeae048e408357e60ea4472ef1a7e
WilChrist <willynzesseu@gmail.com>
2018-03-20 11:36:33 +0000

    Ajout de mon Console.Write et de ma branche

--- ConsolePourSqlLite/Program.cs
+++ ConsolePourSqlLite/Program.cs
@@ -10,6 +10,8 @@ namespace ConsolePourSqlLite
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.Read();
         }
     }
 }
