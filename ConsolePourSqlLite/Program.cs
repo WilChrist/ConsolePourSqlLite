32a536b0e815e841726431e3152cc1f4078e42d7
francisDYL <leroifrancis56@gmail.com>
2018-03-20 11:41:47 +0000

    comm kh

--- ConsolePourSqlLite/Program.cs
+++ ConsolePourSqlLite/Program.cs
@@ -10,7 +10,7 @@ namespace ConsolePourSqlLite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Francis");
+            Console.WriteLine("Kholoud");
         }
     }
 }
